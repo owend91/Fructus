501fd8f89d97dc07e8f296766ed1205c1b3c3373
David Owen <owend91@gmail.com>
2022-12-05 07:26:24 -0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by David Owen on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
